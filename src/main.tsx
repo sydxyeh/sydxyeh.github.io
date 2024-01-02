8db4d216582ae411f29a89242976c7c9e43c2843
Catherine Yeh <catherineyeh@g.harvard.edu>
2024-01-01 20:54:08 -0800

    pushing changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.scss'
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
